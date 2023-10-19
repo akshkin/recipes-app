1f9148f816d864a136db034d4da7a0b96a271ad2
akshkin <107682289+akshkin@users.noreply.github.com>
2023-10-19 20:13:54 +0200

    Structure layout and add Navbar

---
+++ components/navbar/MobileNavbar.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Sheet, SheetClose, SheetContent, SheetTrigger } from "../ui/sheet";
+import Image from "next/image";
+
+import { CATEGORIES } from "@/constants";
+import Link from "next/link";
+
+function MobileNavbar() {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Image
+          src="/assets/icons/hamburger.svg"
+          alt="menu"
+          width={20}
+          height={20}
+          className="cursor-pointer lg:hidden link"
+        />
+      </SheetTrigger>
+      <SheetContent className="border-none">
+        <Link href="/" className="flex flex-col items-center link">
+          <Image src="/assets/logo.svg" alt="logo" width={150} height={100} />
+          <p className="text-center">Recipes</p>
+        </Link>
+        <SheetClose asChild>
+          <div className="flex flex-col gap-3 mt-16">
+            {CATEGORIES.map((category) => (
+              <Link key={category.title} href="/">
+                <p className="uppercase font-bold text-xl text-primary-500 link">
+                  {category.title}
+                </p>
+              </Link>
+            ))}
+          </div>
+        </SheetClose>
+      </SheetContent>
+    </Sheet>
+  );
+}
+
+export default MobileNavbar;
