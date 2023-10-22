f51aab756807e4baf8687a3292703a4b3caa2878
akshkin <107682289+akshkin@users.noreply.github.com>
2023-10-22 16:50:18 +0200

    Create a custom grid class to display recipes

--- components/RecipeCard.tsx
+++ components/RecipeCard.tsx
@@ -12,18 +12,21 @@ function RecipeCard({ title, image }: RecipeProps) {
   return (
     <Link
       href={`/recipe/${title}`}
-      className="p-2 border-[1px] rounded-lg max-lg:w-[150px] min-w-[200px] max-w-[300px] max-lg:h-[300px] h-[400px] "
+      className="flex flex-col p-2 border-[1px] rounded-lg max-lg:w-[150px] min-w-[200px] max-w-[300px] max-lg:h-[300px] h-[400px] hover:scale-105 focus:scale-105 transition-transform"
     >
       <Image
         src={image}
         alt="recipe"
         width={200}
-        height={150}
-        className="w-full object-cover rounded-b-lg"
+        height={120}
+        className="w-full max-lg:h-[220px] h-[300px] object-cover rounded-md"
       />
-      <h2 className="h3 capitalize mt-2 line-clamp-2 text-center">{title}</h2>
+      <h2 className="h3 capitalize mt-2 line-clamp-2 text-center h-[200px]">
+        {title}
+      </h2>
     </Link>
   );
+  ebar;
 }
 
 export default RecipeCard;
