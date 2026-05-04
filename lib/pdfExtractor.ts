e75546cda4c101734ed9a9d9076ef6c5dac21061
Akshaya <akshaya.kini12@gmail.com>
2026-05-04 22:01:35 +0200

    Create functions to extract text from pdf and parse pdf

---
+++ lib/pdfExtractor.ts
@@ -0,0 +1,50 @@
+import * as pdfjsLib from "pdfjs-dist";
+
+/* 
+Set worker path to keep the main thread responsive so that the extraction process 
+occurs in the background (separate thread) without blocking the UI. 
+This is crucial for performance, especially when dealing with large PDF files.
+*/
+// pdfjsLib.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjsLib.version}/pdf.worker.min.js`;
+
+// IMPORTANT: Use the local worker file instead of CDN
+if (typeof window !== "undefined") {
+	pdfjsLib.GlobalWorkerOptions.workerSrc = new URL(
+		"pdfjs-dist/build/pdf.worker.min.mjs",
+		import.meta.url,
+	).toString();
+}
+
+console.log("PDF.js version:", pdfjsLib.version);
+export async function extractTextFromPDF(file: File): Promise<string> {
+	const arrayBuffer = await file.arrayBuffer();
+	const pdf = await pdfjsLib.getDocument({ data: arrayBuffer }).promise;
+
+	let fullText = "";
+
+	// Extract text from all pages. PDF.js uses 1-based indexing for pages.
+	for (let i = 1; i <= pdf.numPages; i++) {
+		const page = await pdf.getPage(i);
+		const textContent = await page.getTextContent();
+		/*
+        getTextContent() returns an object with an 'items' array as follows, 
+        where each item represents a piece of text on the page.
+        {
+            items: [
+                { str: "Chocolate", x: 100, y: 200, ... },
+                { str: "Chip", x: 180, y: 200, ... },
+                { str: "Cookies", x: 100, y: 180, ... },
+                // ... more items
+            ]
+        }
+    */
+		const pageText = textContent.items.map((item: any) => item.str).join(" ");
+		fullText += pageText + "\n";
+	}
+
+	if (fullText.trim().length < 100) {
+		throw new Error("PDF appears to be scanned. Text extraction failed.");
+	}
+
+	return fullText;
+}
---
+++ lib/recipeParser.ts
@@ -0,0 +1,25 @@
+import { GoogleGenAI } from "@google/genai";
+
+const ai = new GoogleGenAI({});
+
+export async function parseRecipeWithGroq(recipeText: string) {
+	const response = await ai.models.generateContent({
+		model: "gemini-3-flash-preview",
+		contents: `Extract the recipe from this text:\n\n${recipeText} and return ONLY valid JSON in this exact format:
+			{
+				"title": "Recipe name",
+				"description": "Brief description",                
+				"ingredients": [
+					{ingredient: "2 cups flour"},
+					{ingredient: "1 tsp salt"}
+				],
+				"method": [
+					{step: "Step 1 instructions"},
+					{step: "Step 2 instructions"}
+				],
+				category: "Dessert",
+				cuisine: "American",
+			}`,
+	});
+	return response.text;
+}
