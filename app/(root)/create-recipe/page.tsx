41c44949f6dc4d0e663fcfe606aa3e64cacea593
Akshaya <akshaya.kini12@gmail.com>
2026-05-04 22:03:38 +0200

    Add the new CreateRecipeContainer to create recipe page

--- app/(root)/create-recipe/page.tsx
+++ app/(root)/create-recipe/page.tsx
@@ -1,22 +1,23 @@
-import CreateRecipeForm from "@/components/forms/CreateRecipeForm";
+import CreateRecipeContainer from "@/components/CreateRecipeContainer";
 import { getMongoUserFromClerkId } from "@/lib/actions/user.action";
 import { auth } from "@clerk/nextjs";
 import React from "react";
 
 async function Page() {
-  const { userId } = auth();
-  if (!userId) {
-    return <h2>Please login to create recipe</h2>;
-  }
+	const { userId } = auth();
+	if (!userId) {
+		return <h2>Please login to create recipe</h2>;
+	}
 
-  const mongoUser = await getMongoUserFromClerkId(userId);
+	const mongoUser = await getMongoUserFromClerkId(userId);
 
-  return (
-    <>
-      <h1 className="text-center mb-10 h1">Create recipe</h1>
-      <CreateRecipeForm type="create" mongoUserId={mongoUser?._id.toString()} />
-    </>
-  );
+	return (
+		<>
+			<h1 className="text-center mb-10 h1">Create recipe</h1>
+			<p>You can even upload a PDF recipe to auto-fill the form!</p>
+			<CreateRecipeContainer mongoUserId={mongoUser?._id.toString()} />
+		</>
+	);
 }
 
 export default Page;
