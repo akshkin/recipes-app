ce42e77826e5348672f4ecda8a2fff125bf1f4b0
Akshaya <akshaya.kini12@gmail.com>
2026-05-11 21:03:38 +0200

    Create a separate client  WelcomeUser component to use in Her

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,27 +1,13 @@
-import { getMongoUserFromClerkId } from "@/lib/actions/user.action";
 import { SignedIn, SignedOut } from "@clerk/nextjs";
-import { auth } from "@clerk/nextjs/server";
 import Link from "next/link";
+import WelcomeUser from "./WelcomeUser";
 
 async function Hero() {
-	const { userId } = await auth();
-	let mongoUser;
-
-	if (userId) {
-		mongoUser = await getMongoUserFromClerkId(userId);
-	}
-
 	return (
-		<div className="flex flex-col items-start justify-center bg-light-800 rounded-lg pl-6 pr-6 w-full max-md:h-[35vh] max-sm:py-3">
-			<h1 className="h1 mb-4">
-				{userId && (
-					<span>
-						Welcome,{" "}
-						<span className="text-accent-500">{mongoUser?.name}!</span>{" "}
-					</span>
-				)}
-			</h1>
-			<p className="mb-4">
+		<div className="flex flex-col items-start justify-center bg-light-800 rounded-lg p-6 w-full max-md:h-[35vh] max-sm:py-3 mx-auto">
+			<WelcomeUser />
+
+			<p className="mb-4 text-lg">
 				Do you love creating recipes and sharing with the world? We now have a
 				feature where you can upload a PDF of your recipe and we will auto-fill
 				the form for you! No more copy-pasting, just upload and create your
