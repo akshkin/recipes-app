b6a1e8db3d3681b10400f9a78d5f2bd7239c7369
Akshaya <akshaya.kini12@gmail.com>
2026-07-31 18:16:23 +0200

    Style the featured recipe

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -89,10 +89,7 @@ function FeaturedRecipe({ recipe }: Recipe) {
 	return (
 		<section className="mb-4 ">
 			<h2 className="h2 mb-3">Featured recipe</h2>
-			<Link
-				href={`/recipe/${title}`}
-				className="flex gap-3 md:h-[300px] max-md:flex-col border rounded-r-md md:rounded-e-md border-gray-300 md:border-l-0"
-			>
+			<div className="flex gap-3 md:h-[300px] max-md:flex-col border rounded-r-md md:rounded-e-md border-gray-300 md:border-l-0">
 				<Image
 					src={`${publicImageUrl}/${image}`}
 					alt=""
@@ -100,16 +97,25 @@ function FeaturedRecipe({ recipe }: Recipe) {
 					height={300}
 					className="w-full md:w-[50%] rounded-t-md md:rounded-s-md object-cover max-md:h-[200px]"
 				/>
-				<div className="md:w-[50%] p-3 md:pt-5">
-					<h3 className="text-3xl font-semibold mb-3">{title}</h3>
-					<p className="text-gray-600 mb-3">{description}</p>
-					<RatingNumber value={averageRating} />
-					<span className={`text-sm text-gray-${ratingsCount ? 700 : 400}`}>
-						({formatNumber(ratingsCount ?? 0)}{" "}
-						{ratingsCount === 1 ? "rating" : "ratings"})
+				<div className="md:w-[50%] p-3 md:pt-5 flex flex-col gap-1 md:gap-2">
+					<span className="border border-accent-500 rounded-3xl text-accent-500 px-2 py-1 text-xs w-fit flex items-center">
+						<Image
+							src="/assets/icons/star.svg"
+							alt={""}
+							width={20}
+							height={20}
+							className="mr-1"
+						/>
+						Editor's pick
 					</span>
+					<h3 className="text-2xl font-semibold">{title}</h3>
+					<p className="text-gray-600 text-sm lg:text-lg">{description}</p>
+
+					<Link href={`/recipe/${title}`} className="btn w-fit text-md">
+						View recipe
+					</Link>
 				</div>
-			</Link>
+			</div>
 		</section>
 	);
 }
