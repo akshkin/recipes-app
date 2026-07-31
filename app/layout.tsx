c125d0c143f8ce8c77b338c88eac119bc2c0fdd7
Akshaya <akshaya.kini12@gmail.com>
2026-07-31 16:22:14 +0200

    Remove cormorant font

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,29 +7,28 @@ const inter = Inter({ subsets: ["latin"] });
 const cormorant = Cormorant({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Recipes App",
-  description: "An app to store and view recipes",
+	title: "Recipes App",
+	description: "An app to store and view recipes",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <ClerkProvider
-      appearance={{
-        elements: {
-          formButtonPrimary: "bg-primary-500",
-          footerActionLink: "text-primary-500 link",
-        },
-      }}
-    >
-      <html lang="en">
-        <body className={`${inter.className} ${cormorant.className}`}>
-          {children}
-        </body>
-      </html>
-    </ClerkProvider>
-  );
+	return (
+		<ClerkProvider
+			appearance={{
+				elements: {
+					formButtonPrimary: "bg-primary-500",
+					footerActionLink: "text-primary-500 link",
+				},
+			}}
+		>
+			<html lang="en">
+				{/* <body className={`${inter.className} ${cormorant.className}`}> */}
+				<body className={`${inter.className}`}>{children}</body>
+			</html>
+		</ClerkProvider>
+	);
 }
