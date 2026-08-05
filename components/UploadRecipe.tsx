b4ce7dd732ede5cc390ec42f21f6d81ed6250ef4
Akshaya <akshaya.kini12@gmail.com>
2026-08-05 15:27:18 +0200

    Add addiitonal fields to recipe form

--- components/UploadRecipe.tsx
+++ components/UploadRecipe.tsx
@@ -2,56 +2,6 @@
 import React, { useRef, useState } from "react";
 import { parseRecipe } from "@/lib/actions/recipe.action";
 
-// const data = {
-// 	title: "Healthy Chocolate Chip Cookies",
-// 	description:
-// 		"The best healthy chocolate chip cookies recipe that yields delicious soft and chewy cookies!",
-// 	ingredients: [
-// 		{ ingredient: "1 cup oat flour" },
-// 		{ ingredient: "1/2 tsp baking soda" },
-// 		{ ingredient: "1/4 tsp salt" },
-// 		{ ingredient: "4 tbsp coconut sugar or brown sugar" },
-// 		{
-// 			ingredient: "4 tbsp white sugar or additional coconut sugar",
-// 		},
-// 		{ ingredient: "1/3 cup chocolate chips or more if desired" },
-// 		{ ingredient: "1/3 cup chopped nuts (optional)" },
-// 		{ ingredient: "1 tsp pure vanilla extract" },
-// 		{ ingredient: "2 tbsp vegetable or melted coconut oil" },
-// 		{ ingredient: "3-5 tbsp milk of choice, as needed" },
-// 	],
-// 	method: [
-// 		{ step: "Preheat oven to 380 degrees." },
-// 		{ step: "Combine dry ingredients and mix very well." },
-// 		{ step: "Add wet ingredients and form into a big ball." },
-// 		{ step: "Form little balls from the big ball of dough." },
-// 		{
-// 			step: "For soft cookies, refrigerate until cold; otherwise, bake right away.",
-// 		},
-// 		{ step: "Bake for 9 minutes." },
-// 		{ step: "Form little balls from the big ball of dough." },
-// 		{
-// 			step: "For soft cookies, refrigerate until cold; otherwise, bake right away.",
-// 		},
-// 		{ step: "Bake for 9 minutes." },
-// 		{ step: "Bake for 9 minutes." },
-// 		{
-// 			step: "Remove from oven when they’re still a little undercooked and let cool for 10 minutes on the tray, as they will con",
-// 		},
-// 		{
-// 			step: "Remove from oven when they’re still a little undercooked and let cool for 10 minutes on the tray, as they will continue to cook while cooling.",
-// 		},
-// 		{
-// 			step: "If the cookies have not spread out, smush them down with a spoon.",
-// 		},
-// 		{
-// 			step: "Store in a lidded plastic container for softer cookies or a lidded glass container for crispier cookies.",
-// 		},
-// 	],
-// 	category: "Dessert",
-// 	cuisine: "American",
-// };
-
 function UploadRecipe({
 	setPrefilledForm,
 }: {
@@ -98,6 +48,7 @@ function UploadRecipe({
 				}
 			}
 		} catch (err) {
+			console.log(err);
 			setError(
 				"Failed to extract recipe. Please try again after some time. Possible reasons could be that the PDF is scanned or the text is in a language other than English.",
 			);
@@ -111,7 +62,7 @@ function UploadRecipe({
 	};
 
 	return (
-		<div className="border-2 border-dashed rounded-lg p-6 mx-8 my-4">
+		<div className="border-2 border-dashed rounded-lg p-6 my-4">
 			<label className="cursor-pointer">
 				<input
 					ref={fileInputRef}
