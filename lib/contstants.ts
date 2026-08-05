86ace5bb616676f8d61c52085111c96a86e62919
Akshaya <akshaya.kini12@gmail.com>
2026-08-05 20:42:21 +0200

    Create a dietary tags constant

--- lib/contstants.ts
+++ lib/contstants.ts
@@ -1,2 +1,62 @@
+import {
+	Bone,
+	Drumstick,
+	Fish,
+	Leaf,
+	LeafyGreen,
+	MilkOff,
+	NutOff,
+	Salad,
+	Wheat,
+	WheatOff,
+} from "lucide-react";
+
 const bucketUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 export const publicImageUrl = `${bucketUrl}/storage/v1/object/public/recipe`;
+
+export const dietaryTagsConst: Record<string, { icon: any; color: string }> = {
+	Vegetarian: {
+		icon: LeafyGreen,
+		color: "text-green-600",
+	},
+
+	Vegan: {
+		icon: Leaf,
+		color: "text-emerald-600",
+	},
+
+	"Gluten-Free": {
+		icon: WheatOff,
+		color: "text-yellow-600",
+	},
+
+	Pescatarian: {
+		icon: Fish,
+		color: "text-blue-500",
+	},
+
+	Keto: {
+		icon: Drumstick,
+		color: "text-orange-500",
+	},
+
+	Paleo: {
+		icon: Bone,
+		color: "text-orange-500",
+	},
+
+	"Low-Fat": {
+		icon: Salad,
+		color: "text-orange-500",
+	},
+
+	"Nut-Free": {
+		icon: NutOff,
+		color: "text-orange-500",
+	},
+
+	"Dairy-Free": {
+		icon: MilkOff,
+		color: "text-white",
+	},
+};
