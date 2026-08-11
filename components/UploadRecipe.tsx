2d5a17d208f45d43bc8878011df86f22ea22b405
Akshaya <akshaya.kini12@gmail.com>
2026-08-11 09:39:55 +0200

    Disable form when extracting recipe

--- components/UploadRecipe.tsx
+++ components/UploadRecipe.tsx
@@ -4,8 +4,10 @@ import { parseRecipe } from "@/lib/actions/recipe.action";
 
 function UploadRecipe({
 	setPrefilledForm,
+	setIsRecipeExtracting,
 }: {
 	setPrefilledForm: (data: string) => void;
+	setIsRecipeExtracting: React.Dispatch<React.SetStateAction<boolean>>;
 }) {
 	const [loading, setLoading] = useState(false);
 	const [error, setError] = useState("");
@@ -28,13 +30,14 @@ function UploadRecipe({
 		const { extractTextFromPDF } = await import("@/lib/pdfExtractor");
 		try {
 			setLoading(true);
+			setIsRecipeExtracting(true);
 			setError("");
 
 			// Extract text from PDF
 			const text = await extractTextFromPDF(file);
 
 			// Parse with LLM
-			const response = await parseRecipe(text);
+			const response = await parseRecipe(text.text);
 			if (response) {
 				setPrefilledForm(response);
 
@@ -54,6 +57,7 @@ function UploadRecipe({
 			);
 		} finally {
 			setLoading(false);
+			setIsRecipeExtracting(false);
 			// Clear the file input value to allow re-uploading the same file if needed
 			if (fileInputRef.current) {
 				fileInputRef.current.value = "";
@@ -74,7 +78,7 @@ function UploadRecipe({
 				/>
 				<div className="text-center">
 					{loading ? (
-						<p>Extracting recipe...</p>
+						<p className="animate-pulse">Extracting recipe...</p>
 					) : (
 						<>
 							<p className="text-lg font-medium">
@@ -96,9 +100,9 @@ function UploadRecipe({
 				fields as those are not usually populated automatically and the image
 				would have to be manually uploaded.
 			</p>
-			<p className="text-sm text-gray-500 mt-2">
+			{/* <p className="text-sm text-gray-500 mt-2">
 				Right now this feature only supports English recipes.
-			</p>
+			</p> */}
 			{error && (
 				<div>
 					<p className="text-red-500 text-sm mt-2">{error}</p>
